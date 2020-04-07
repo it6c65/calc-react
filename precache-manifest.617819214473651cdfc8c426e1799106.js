404c1d8424d9e8595edf259b6933a1f67e683515
Luis Ilarraza <ilarrazaluis82@gmail.com>
2020-04-07 05:05:11 -0400

    Updates

---
+++ precache-manifest.617819214473651cdfc8c426e1799106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0168b28d54d08930f8ea31294c176186",
+    "url": "/calc-react/index.html"
+  },
+  {
+    "revision": "de201334a721f5d408e1",
+    "url": "/calc-react/static/css/main.06f5d628.chunk.css"
+  },
+  {
+    "revision": "d5566dab28f842b64e63",
+    "url": "/calc-react/static/js/2.6afc0680.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-react/static/js/2.6afc0680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de201334a721f5d408e1",
+    "url": "/calc-react/static/js/main.062fdbba.chunk.js"
+  },
+  {
+    "revision": "c5d0d5ddf2f4d881bcf7",
+    "url": "/calc-react/static/js/runtime-main.389a045f.js"
+  }
+]);
